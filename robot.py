2a692824d1175df8b917c2fb0e51d3e99e575fa5
Kamil Łepek <kamil.lepek94@gmail.com>
2018-09-18 15:02:50 +0000

    Implemented sending frames via UDP

--- robot.py
+++ robot.py
@@ -1,29 +1,52 @@
 import cv2
-import time
-
-print(cv2.__version__)
+import datetime
+import sys
+from socket import *
 
 cap = cv2.VideoCapture(0)
-cap.set(3,320);
-cap.set(4,240);
+cap.set(3,320); # sets height of the frame
+cap.set(4,240); # sets width of the frame
 
-start_time = time.time()
-interval = 1 # displays the frame rate every 1 second
-counter = 0
+host = "192.168.0.19"
+port = 4096
+addr = (host, port)
+buf = 1024 # size of buffer to send files
 
-while(True):
-    ret,frame = cap.read()
-    if ret == True:
-        counter+=1
+if (len(sys.argv) > 1):
+    input = sys.argv[1]
 
-    cv2.imshow('DjRoomba',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    
-    if (time.time() - start_time) > interval :
-        print("FPS: ", counter / (time.time() - start_time))
-        counter = 0
-        start_time = time.time()
-        
-cap.release()
-cv2.destroyAllWindows()
+
+def sendFile(fName):
+    sock = socket(AF_INET, SOCK_DGRAM) # AF_INET - ipv4 protocol, SOCK_DGRAM - UDP
+    sock.sendto(fName, addr)
+
+    frame = open(fName, "rb")
+    data = frame.read(buf)
+
+    print("Sending data.." + str(datetime.datetime.now()))
+
+    while data:
+        if(sock.sendto(data, addr)):
+            data = frame.read(buf)
+    frame.close()
+    sock.close()
+
+def mainLoop():
+    while(cap.isOpened()):
+        ret,frame = cap.read() # capture frame
+        if ret == True:
+            cv2.imwrite("frame.jpg", frame)
+            if input == "debug":
+                cv2.imshow('frame', frame)
+            sendFile("frame.jpg")
+            count = 0
+        else:
+            break;
+        if cv2.waitKey(1) & 0xFF == ord('q'): # exit if 'q' pressed
+            break
+
+if __name__ == '__main__':
+    print(cv2.__version__)
+    mainLoop()
+    cap.release()
+    cv2.destroyAllWindows()
